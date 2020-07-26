4702196af9d250766bc65abea98e2bdbffbbd1e5
liuyi <liuyi@hetao101.com>
2020-07-27 03:15:40 +0800

    大文件分片上传

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # upload
-大文件上传
+大文件分片上传
 
 ## 安装
 
@@ -15,7 +15,45 @@ import Upload from '@stroll/upload'
 Vue.use(Upload)
 
 ```
+## 初始化参数
+````js
+basicData: {
+  useName: '', // 可选择加盐后名称 saltName 或原本名称 name ，默认使用 name
+  salt: 'salt', // 用于文件名称加盐
+  multiple: true, // 是否允许多选，默认可以多选
+  chunkSize: 2 * 1024 * 1024, // 分片大小
+  amount: 4, // 上传数量
+  accept: false, // 上传类型
+  typeConfig: [ // 上传类型细化配置
+    {
+      amount: 2, // 类型上传数量
+      accept: 'mp4,mp3' // 类型
+    }
+  ],
+  uploadButton: { // 上传按钮配置
+    style: '', // 上传按钮样式
+    show: false // 上传按钮是否显示，默认不显示
+  },
+  selectButton: { // 选择文件按钮配置
+    style: '' // 选择文件按钮样式
+  },
+  upload: { // 分片上传接口配置
+    method: 'post', // 分片上传接口方式，默认 post
+    url: '/file/upload' // 分片上传接口地址
+  },
+  confirm: { // 上传完成接口配置
+    method: 'post', // 上传完成接口方式，默认 post
+    url: '/file/merge_chunks' // 上传完成接口地址
+  },
+  status: { code: 0, status: 'success', message: '成功' } // 成功回调参数
+}
+````
 ## 调用
 ```html
-
+<sUpload
+  @onsubmit='submit' // 上传文件自定义，可不传
+  @onselect='select' // 选择文件错误回调，可不传
+  :uploadFileInfo='uploadFileInfo' // 上传文件信息
+  :configData='basicData' // 初始化
+/>
 ```
--- src/index.vue
+++ src/index.vue
@@ -1,15 +1,7 @@
 <template>
   <div class="uploadBox" style="display: inline-block;">
-    <div class="selectButton" >
-      <button :style="basicData.selectButton.style">选择文件</button>
-      <input
-        type="file"
-        id="stroll-file"
-        title=" "
-        :multiple="basicData.multiple"
-        :accept="basicData.accept"
-        @change="fileChange"
-      >
+    <div class="selectButton" id="stroll-uploadBox">
+      <button :style="basicData.selectButton.style" @click="selectButton">选择文件</button>
     </div>
     <button v-if="basicData.uploadButton.show" :style="basicData.uploadButton.style" class="uploadButton" @click="submitBtn()">上传</button>
     <div style="clear: both;"></div>
@@ -19,7 +11,7 @@
           {{isUploadStatus[item.isUpload || 'default'].val}}
         </span>
         <div class="fl mr10">
-          <span>{{item.name}}</span>
+          <span>{{item[basicData.useName || 'name']}}</span>
 
           <div
             class="schedule"
@@ -71,14 +63,19 @@ export default {
         fail: { val: '✘', color: 'red' }
       },
       basicData: {
+        useName: '',
         salt: 'salt',
         multiple: true,
         chunkSize: 2 * 1024 * 1024,
-        amount: 2,
+        amount: 4,
         accept: false,
+        typeConfig: [
+          { amount: 2, accept: 'mp4,mp3' },
+          { amount: 2, accept: 'jpg' }
+        ],
         uploadButton: {
           style: '',
-          show: true
+          show: false
         },
         selectButton: {
           style: ''
@@ -92,7 +89,8 @@ export default {
           url: '/file/merge_chunks'
         },
         status: { code: 0, status: 'success', message: '成功' }
-      }
+      },
+      deep: ['upload', 'confirm', 'selectButton', 'uploadButton', 'typeConfig']
     }
   },
   mounted () {
@@ -102,7 +100,7 @@ export default {
   methods: {
     initConfig (params) {
       Object.keys(params).forEach(item => {
-        if (['upload', 'confirm'].includes(item)) {
+        if (this.deep.includes(item)) {
           Object.keys(params[item]).forEach(key => {
             this.basicData[item][key] = params[item][key]
           })
@@ -115,86 +113,117 @@ export default {
       this.file = null
       this.uploadFileInfo.fileList = []
     },
-    async selectCallback (res) {
-      this.$emit('selectCallback', res)
+
+    selectButton () {
+      const strollUploadBox = document.getElementById('stroll-uploadBox')
+      const inputFile = document.createElement('input')
+      inputFile.setAttribute('type', 'file')
+      inputFile.setAttribute('multiple', this.basicData.multiple)
+      inputFile.setAttribute('accept', this.basicData.accept)
+      inputFile.setAttribute('title', '')
+
+      strollUploadBox.appendChild(inputFile)
+
+      inputFile.click()
+      inputFile.onchange = async (e) => {
+        await this.selectChange(e)
+        if (!this.basicData.uploadButton.show) {
+          await this.submitBtn()
+        }
+      }
     },
-    async delFile (row) {
-      const arr = []
-      this.file.forEach(item => {
-        if (item.name !== row.name) {
-          arr.push(item)
+    async selectChange (e) {
+      
+      const filesLeng = e.target.files.length
+      const fileListLeng = this.uploadFileInfo.fileList.length
+      if ((filesLeng + fileListLeng) > this.basicData.amount || fileListLeng >= this.basicData.amount) {
+        this.Callback(this.getStatus(1001), 'select')
+        return false
+      }
+
+      const getTypeLeng = await this.getTypeLeng(e.target.files)
+      let beyondType = {}
+      const amount = Object.keys(getTypeLeng.typeObject).every(key => {
+        beyondType = {
+          key,
+          amount: getTypeLeng.typeConfigLing[key]
+        }
+        if (getTypeLeng.typeObject[key] <= getTypeLeng.typeConfigLing[key]) {
+          return true
+        } else {
+          return false
         }
       })
+      if (!amount) {
+        await this.Callback(this.getStatus(1003, beyondType), 'select')
+        return
+      }
+      
+      return await this.addUploadList(e.target.files)
+    },
+    async addUploadList (files) {
+      const nameArr = await this.nameArr()
+      files.forEach(async (item) => {
+        await this.defineProperty(item)
+
+        if (!nameArr.includes(item.name)) {
+          this.uploadFileInfo.fileList.push({
+            name: item.name,
+            saltName: item.saltName,
+            size: item.size,
+            isUpload: false,
+            schedule: 0
+          })
+        } else {
+          await this.Callback(this.getStatus(1002, {
+            name: item.name,
+            saltName: item.saltName,
+            size: item.size
+          }), 'select')
+          return false
+        }
+      })
+    },
+    async delFile (row) {
       this.uploadFileInfo.fileList.forEach((item, index) => {
         if (item.name === row.name) {
           this.uploadFileInfo.fileList.splice(index, 1)
         }
       })
-      this.file = arr
-      this.$emit('delFile', row, this.file)
     },
-    async fileChange (e) {
-      if (!e.target.files || !e.target.files[0]) return
-      if ((+e.target.files.length + +this.uploadFileInfo.fileList.length) > this.basicData.amount || this.uploadFileInfo.fileList.length >= this.basicData.amount) {
-        await this.selectCallback(this.getStatus(1001))
-        return false
-      }
-      for (var i = 0; i < e.target.files.length; i++) {
-        const FileName = e.target.files[i].name
-        Object.defineProperty(e.target.files[i], 'name', {
-          writable: true //设置属性为可写
+    async submitBtn () {
+      const inputEl = await this.getInput()
+      inputEl.forEach(el => {
+        el.files.forEach(item => {
+          this.submit(item)
         })
-        e.target.files[i].name = `${this.basicData.salt}${FileName}`
-      }
-      this.file = e.target.files
-      const inputFile = document.getElementById('stroll-file')
-      inputFile.setAttribute('type', 'text')
-      inputFile.setAttribute('type', 'file')
-      await this.getFile(this.file)
-    },
-    async getFile (file) {
-      file && file.forEach(async item => {
-        const reader = new FileReader()
-        reader.readAsArrayBuffer(item)
-        reader.onload = async e => {
-          this.uploadFileInfo.fileList.push({
-            name: item.name,
-            size: item.size,
-            isUpload: false,
-            fileHash: e.target.result,
-            schedule: 0
-          })
-          if (!this.basicData.uploadButton.show) {
-            await this.submit(item)
-          }
-        }
-        reader.onerror = () => {
-          console.warn('文件读取失败！')
-        }
-      })
-    },
-    async submitBtn (file) {
-      file = file || this.file
-      file && this.file.forEach(item => {
-        this.submit(item)
       })
     },
     async reUpload (params) {
-      this.file && this.file.forEach(item => {
-        this.uploadFileInfo.fileList.forEach(el => {
-          if (item.name === params.name && params.name === el.name) {
-            el.isUpload = false
-            el.schedule = 0
-            this.submit(item)
-          }
+      const inputEl = await this.getInput()
+      inputEl.forEach(el => {
+        el.files.forEach(item => {
+          this.uploadFileInfo.fileList.forEach(async val => {
+            if (item.name === params.name && params.name === val.name) {
+              val.isUpload = false
+              val.schedule = 0
+              await this.defineProperty(item, { isUpload: false, schedule: 0 })
+              this.submit(item)
+            }
+          })
         })
       })
     },
-    async submit (item) {
-      const file = item || this.file[0]
-      if (!file) {
+    async submit (file) {
+      if (!file || file.isUpload) {
         return
       }
+      const nameArr = await this.nameArr()
+      if (this.$listeners.onsubmit) {
+        this.$emit('onsubmit', { file, nameArr})
+        return
+      }
+      if (!nameArr.includes(file.name)) return
       const axiosPromiseArray = []
       const fileSize = file.size
       const chunks = Math.ceil(fileSize / this.basicData.chunkSize)
@@ -233,28 +262,55 @@ export default {
           fileHash: MD5(file.name)
         }
         await axios[this.basicData.confirm.method](this.basicData.confirm.url, data).then(() => {
-          this.uploadFileInfo.fileList.forEach(item => {
+          this.uploadFileInfo.fileList.forEach(async item => {
             if (file.name === item.name) {
               item.isUpload = 'success'
+              await this.defineProperty(file, { isUpload: 'success', schedule: 100 })
             }
           })
         }).catch(err => {
-          this.uploadFileInfo.fileList.forEach(item => {
+          this.uploadFileInfo.fileList.forEach(async item => {
             if (file.name === item.name) {
               item.isUpload = 'fail'
+              await this.defineProperty(file, { isUpload: 'fail', schedule: 100 })
             }
           })
           console.log(err)
         })
+        if (this.uploadFileInfo.fileList.every(item => item.isUpload === 'success')) {
+          this.delFileInput()
+        }
       }
-      await axios.all(axiosPromiseArray).then(async () => {
-        mergeChunks()
+      await axios.all(axiosPromiseArray).then(async res => {
+        mergeChunks(res)
+
       }).catch(async (err) => {
-        mergeChunks()
+        mergeChunks(err)
         console.log(err)
       })
     },
-    async getStatus (code) {
+    async delFileInput () {
+      const inputEl = await this.getInput()
+      inputEl[0] && inputEl[0].parentNode.removeChild(inputEl[0])
+      if (inputEl.length > 0) this.delFileInput()
+    },
+    async defineProperty (item, Reset) {
+      const { schedule, isUpload } = Reset || {}
+      const FileName = item.name
+      !Reset && Object.defineProperty(item, 'saltName', {
+        value: `${this.basicData.salt}${FileName}`,
+        writable: true //设置属性为可写
+      })
+      Object.defineProperty(item, 'schedule', {
+        value: schedule || 0,
+        writable: true
+      })
+      Object.defineProperty(item, 'isUpload', {
+        value: isUpload || false,
+        writable: true
+      })
+    },
+    async getStatus (code, item) {
       let row = { code: 0, status: 'success', message: '成功' }
       switch (code) {
         case 1001:
@@ -264,10 +320,101 @@ export default {
             message: `上传数量超出，上限为${this.basicData.amount}`
           }
           break
+        case 1002:
+          row = {
+            code: 1002,
+            status: 'error',
+            message: `文件 ${item.neme} 重复上传`,
+            errFile: item
+          }
+          break
+        case 1003:
+          row = {
+            code: 1003,
+            status: 'error',
+            message: `上传文件类型 ${item.key} 数量超出，上限为${item.amount}`,
+            errFile: item
+          }
+          break
         default:
           row = { code: 0, status: 'success', message: '成功' }
       }
       return row
+    },
+    async getInput () {
+      const strollUploadBox = document.getElementById('stroll-uploadBox')
+      const fileInput = strollUploadBox.getElementsByTagName("input")
+      return fileInput
+    },
+    async nameArr () {
+      return this.uploadFileInfo.fileList.map(item => item.name)
+    },
+    async md5Hash (result) {
+      return new Promise((resolve, reject) => {
+        try {
+          const md5Hash = MD5(result)
+          resolve(md5Hash)
+        } catch (error) {
+          reject(error)
+        }
+      })
+    },
+    async getTypeLeng (files) {
+      return new Promise((resolve, reject) => {
+        try {
+          const filesArr = []
+          const typeObject = {}
+          files.forEach(item => {
+            const nameArr = item.name.split('.')
+            filesArr.push(nameArr[nameArr.length - 1])
+          })
+          const fileListArr = this.uploadFileInfo.fileList.map(item => {
+            const nameArr = item[this.basicData.useName || 'name'].split('.')
+            return nameArr[nameArr.length - 1]
+          })
+          const filesTypeLeng = filesArr.reduce(function(prev,next){
+            prev[next] = (prev[next] + 1) || 1
+            return prev
+          }, {})
+          const filesListLeng = fileListArr.reduce(function(prev,next){
+            prev[next] = (prev[next] + 1) || 1
+            return prev
+          }, {})
+          let typeConfigLing = null
+          if (this.basicData.typeConfig && this.basicData.typeConfig.length && this.basicData.typeConfig[0].accept) {
+            typeConfigLing = this.basicData.typeConfig.reduce(function(prev,next){
+              prev[next.accept] = next.amount || 0
+              return prev
+            }, {})
+          }
+          if (typeConfigLing) {
+            Object.keys(typeConfigLing).forEach(key => {
+              typeObject[key] = 0
+              const typeArr = key.split(',')
+              Object.keys(filesTypeLeng).forEach(item => {
+                if (typeArr.includes(item)) {
+                  typeObject[key] = typeObject[key] + filesTypeLeng[item]
+                }
+              })
+              Object.keys(filesListLeng).forEach(item => {
+                if (typeArr.includes(item)) {
+                  typeObject[key] = typeObject[key] + filesListLeng[item]
+                }
+              })
+            })
+          }
+          resolve({ typeObject, typeConfigLing })
+        } catch (error) {
+          reject(error)
+        }
+      })
+    },
+    async Callback (res, type) {
+      if (this.$listeners.onselect && type === 'select') {
+        this.$emit('onselect', res)
+        return
+      }
+      console.log(res.message)
     }
   }
 }
@@ -292,10 +439,10 @@ export default {
   border-color: #57a3f3;
 }
 .selectButton input {
-  position: absolute;
-  font-size: 200px;
-  right: 0;
-  top: 0;
+  position: fixed;
+  /* font-size: 200px; */
+  right: -1000px;
+  top: -10000;
   opacity: 0;
 }
 .selectButton {
